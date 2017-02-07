9994987e8e8793432364282dd29cc70383cce55e
hvucic <hrvoje.vucic@hotmail.com>
2017-02-07 14:31:08 +0100

    Optimizing

--- redirect_middleware/middleware.py
+++ redirect_middleware/middleware.py
@@ -2,10 +2,9 @@ from django.http import HttpResponseRedirect
 
 
 class UrlRedirectMiddleware:
+    blacklist = ('/register', '/course', '/u/', '/account', '/search', '/about', '/blog', '/contact', '/donate')
 
     def process_request(self, request):
-        blacklist = ('/register', '/course', '/u/', '/account', '/search', '/about', '/blog', '/contact', '/donate')
-
         url_path = request.META['PATH_INFO']
         protocol = "https://" if request.is_secure() else "http://"
         redirect_url = "%s%s/dashboard" % (protocol, request.META['HTTP_HOST'])
@@ -13,6 +12,6 @@ class UrlRedirectMiddleware:
         if url_path == '/':
             return HttpResponseRedirect(redirect_url)
         else:
-            for str in blacklist:
+            for str in self.blacklist:
                 if url_path.find(str) is 0:
                     return HttpResponseRedirect(redirect_url)
