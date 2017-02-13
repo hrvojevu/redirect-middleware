107fc7de60d8461bc8eace0bec81b79000cd5cfa
hvucic <hrvoje.vucic@hotmail.com>
2017-02-13 15:53:24 +0100

    Middleware update

--- redirect_middleware/middleware.py
+++ redirect_middleware/middleware.py
@@ -7,11 +7,14 @@ class UrlRedirectMiddleware:
     def process_request(self, request):
         url_path = request.META['PATH_INFO']
         protocol = "https://" if request.is_secure() else "http://"
-        redirect_url = "%s%s/dashboard" % (protocol, request.META['HTTP_HOST'])
+        dashboard = "%s%s/dashboard" % (protocol, request.META['HTTP_HOST'])
+        oidc = "%s%s/auth/login/oidc-oauth2/?auth_entry=login&next=%%2Fdashboard" % (protocol, request.META['HTTP_HOST'])
 
-        if url_path == '/':
-            return HttpResponseRedirect(redirect_url)
+        if url_path.find('/login') is 0:
+            return HttpResponseRedirect(oidc)
+        elif url_path == '/':
+            return HttpResponseRedirect(dashboard)
         else:
             for str in self.blacklist:
                 if url_path.find(str) is 0:
-                    return HttpResponseRedirect(redirect_url)
+                    return HttpResponseRedirect(dashboard)
